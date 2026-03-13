29d014848b800db44f90b049457c2c9c91944134
dip1905 <dipesh.patel9977@gmail.com>
2026-03-13 12:04:01 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api';
+//const BASE_URL = 'http://localhost:8080/api';
 const API_BASE_URL = 'https://inventory-backend-attf.onrender.com/api';
 
 const api = axios.create({
